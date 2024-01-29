dbad7d310a5158f9907824802c23e56e27db22c5
Agustin Casari Bacas <agustincasaribacas@hotmail.com>
2024-01-29 14:01:23 -0300

    s

--- index.js
+++ index.js
@@ -1,131 +1,131 @@
-const express = require('express');
-const app = express();
-const cors = require('cors');
-const mongoose = require('mongoose');
-const Local = require('./models/local');
-const morgan = require('morgan');
-const dotenv = require("dotenv");
-dotenv.config();
-const AppError = require('./AppError');
-const companiesRoutes = require('./routes/companies');
-const userRoutes = require('./routes/user');
-const session = require('express-session');
-const db_url = process.env.MONGO_URI;
-const secret = process.env.secret;
-const jwt = require('jsonwebtoken');
+    const express = require('express');
+    const app = express();
+    const cors = require('cors');
+    const mongoose = require('mongoose');
+    const Local = require('./models/local');
+    const morgan = require('morgan');
+    const dotenv = require("dotenv");
+    dotenv.config();
+    const AppError = require('./AppError');
+    const companiesRoutes = require('./routes/companies');
+    const userRoutes = require('./routes/user');
+    const session = require('express-session');
+    const db_url = process.env.MONGO_URI;
+    const secret = process.env.secret;
+    const jwt = require('jsonwebtoken');
 
-app.use(session({secret: 'notagoodsecret'}))
-mongoose.set('strictQuery', true)
-app.use(express.json()); //sirve para tratar el body del post si viene por json
+    app.use(session({secret: 'notagoodsecret'}))
+    mongoose.set('strictQuery', true)
+    app.use(express.json()); //sirve para tratar el body del post si viene por json
 
-const options = {
-    origin: ['https://gym-routines-app.vercel.app', 'http://localhost:3000'],
-}
-app.use(cors(options))
+    const options = {
+        origin: ['https://gym-routines-app.vercel.app', 'http://localhost:3000'],
+    }
+    app.use(cors(options))
 
-app.use((req, res, next) => {
-    if (req.path !== '/user/login') {
-        try{
-            const token = req.headers.authorization.split(" ")[1];
-            const payload = jwt.verify(token, secret);
-            if(payload && (payload.exp > Date.now())) {
-                console.log('success', new Date(payload.exp), new Date(Date.now()))
-                next();
-            }else {
-                res.status(401).send({message: 'Unauthorized'});
+    app.use((req, res, next) => {
+        if (req.path !== '/user/login') {
+            try{
+                const token = req.headers.authorization.split(" ")[1];
+                const payload = jwt.verify(token, secret);
+                if(payload && (payload.exp > Date.now())) {
+                    console.log('success', new Date(payload.exp), new Date(Date.now()))
+                    next();
+                }else {
+                    res.status(401).send({message: 'Unauthorized'});
+                }
+            }catch(err) {
+                res.status(401).send({error: err.message});
             }
-        }catch(err) {
-            res.status(401).send({error: err.message});
+        }else {
+            next();
         }
-    }else {
-        next();
+    })
+
+    app.use('/companies', companiesRoutes);
+    app.use('/user', userRoutes);
+
+    main().catch(err => console.log(err, 'Errorrrrrrrrrrr'));
+
+    async function main() {
+
+    await mongoose.connect(db_url);
+    
+    console.log('Mongoose open');
+    //'mongodb://127.0.0.1:27017/gymRoutinesApp'
+    // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
     }
-})
 
-app.use('/companies', companiesRoutes);
-app.use('/user', userRoutes);
-
-main().catch(err => console.log(err, 'Errorrrrrrrrrrr'));
-
-async function main() {
-
-  await mongoose.connect(db_url);
-  
-  console.log('Mongoose open');
-  //'mongodb://127.0.0.1:27017/gymRoutinesApp'
-  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
-}
-
-const verifyPass = (req, res, next) => {
-    const { password } = req.query;
-    if(password === 'pepe') {
-        next();
-    }else{
-        res.status(401);
-        throw new AppError('Password is required', 401);
+    const verifyPass = (req, res, next) => {
+        const { password } = req.query;
+        if(password === 'pepe') {
+            next();
+        }else{
+            res.status(401);
+            throw new AppError('Password is required', 401);
+        }
     }
-}
 
-const handleValidationError = err => {
-    console.log(err);
-    return new AppError(`Validation failed...${err.message}`, 400);
-}
-//status 403 si tenes token pero no sos admin
-app.use(morgan('tiny'))
-
-app.use((req, res, next) => {
-    req.requestTime = Date.now();
-    console.log('Incoming request!');
-    return next(); //next sera obligatorio para continuar el proceso e intentar matchear con una ruta. el return culmina la ejecucion, por lo  que despues pasara al siguiente middleware si o si.
-})
-
-//los middlewares se pueden personalizar solo para una ruta especifica aclarandola como primer parametro y segundo el callback
-
-app.use(express.urlencoded({ extended: true })); //sirve para tratar el body del post si viene por form
-app.use(express.json()); //sirve para tratar el body del post si viene por json
-
-/*app.get('/', (req, res) => {
-    res.send('Home page!');
-})*/
-
-app.get('/password', verifyPass, async (req, res) => {
-    console.log('Ypu are logged in!');
-})
-
-// Local crud
-
-app.get('/locals', async (req, res) => {
-    const locals = await Local.find({});
-    res.send(locals);
-})
-
-app.get('/locals/:id', async (req, res) => {
-    const { id } = req.params;
-    const local = await Local.findById(id);
-    res.send(local);
-})
-
-app.post('/locals/new', async (req, res) => {
-    const locals = await Company.find({});
-    res.send(companies);
-})
-
-app.use((req, res) => {
-    res.status(404).send('Not found');
-})
-
-//cuando se quiere manejar errores en un middleware se deben agregar cuatro parametros donde el primero es el error.
-app.use((err, req, res, next) => {
-    console.log('*************************');
-    console.log('**********ERROR**********');
-    console.log('*************************');
-    console.log(err.name);
-    if(err.name === 'ValidationError'){
-        err = handleValidationError(err);
+    const handleValidationError = err => {
+        console.log(err);
+        return new AppError(`Validation failed...${err.message}`, 400);
     }
-    next(err);
-})
+    //status 403 si tenes token pero no sos admin
+    app.use(morgan('tiny'))
 
-app.listen(8080, () => {
-    console.log('Listening on port 8080!!!');
-})
+    app.use((req, res, next) => {
+        req.requestTime = Date.now();
+        console.log('Incoming request!');
+        return next(); //next sera obligatorio para continuar el proceso e intentar matchear con una ruta. el return culmina la ejecucion, por lo  que despues pasara al siguiente middleware si o si.
+    })
+
+    //los middlewares se pueden personalizar solo para una ruta especifica aclarandola como primer parametro y segundo el callback
+
+    app.use(express.urlencoded({ extended: true })); //sirve para tratar el body del post si viene por form
+    app.use(express.json()); //sirve para tratar el body del post si viene por json
+
+    /*app.get('/', (req, res) => {
+        res.send('Home page!');
+    })*/
+
+    app.get('/password', verifyPass, async (req, res) => {
+        console.log('Ypu are logged in!');
+    })
+
+    // Local crud
+
+    app.get('/locals', async (req, res) => {
+        const locals = await Local.find({});
+        res.send(locals);
+    })
+
+    app.get('/locals/:id', async (req, res) => {
+        const { id } = req.params;
+        const local = await Local.findById(id);
+        res.send(local);
+    })
+
+    app.post('/locals/new', async (req, res) => {
+        const locals = await Company.find({});
+        res.send(companies);
+    })
+
+    app.use((req, res) => {
+        res.status(404).send('Not found');
+    })
+
+    //cuando se quiere manejar errores en un middleware se deben agregar cuatro parametros donde el primero es el error.
+    app.use((err, req, res, next) => {
+        console.log('*************************');
+        console.log('**********ERROR**********');
+        console.log('*************************');
+        console.log(err.name);
+        if(err.name === 'ValidationError'){
+            err = handleValidationError(err);
+        }
+        next(err);
+    })
+
+    app.listen(8080, () => {
+        console.log('Listening on port 8080!!!');
+    })
--- routes/user.js
+++ routes/user.js
@@ -1,104 +1,104 @@
-const express = require('express');
-const router = express.Router();
-const User = require('../models/user');
-const { wrapAsync } = require('../utils');
-const bcrypt = require('bcrypt');
-const AppError = require('../AppError');
-const jwt = require('jsonwebtoken');
+    const express = require('express');
+    const router = express.Router();
+    const User = require('../models/user');
+    const { wrapAsync } = require('../utils');
+    const bcrypt = require('bcrypt');
+    const AppError = require('../AppError');
+    const jwt = require('jsonwebtoken');
 
-const secret = process.env.SECRET;
+    const secret = process.env.SECRET;
 
-//si necesito usar un middleware utilizo router.use(el middleware)
+    //si necesito usar un middleware utilizo router.use(el middleware)
 
-// User crud
+    // User crud
 
-const hashPassword = async (pass) => {
-    const salt = await bcrypt.genSalt(12);
-    const hash = await bcrypt.hash(pass, salt);
-    return hash;
-} 
+    const hashPassword = async (pass) => {  
+        const salt = await bcrypt.genSalt(12);
+        const hash = await bcrypt.hash(pass, salt);
+        return hash;
+    } 
 
-router.get('/', async (req, res) => {
-    const users = await User.find({});
-    const filteredUsersData = users.map(user => ({ username: user.username, weight: user.weight || null, height: user.height || null, email: user.email || '', id: user._id }));
-    console.log(`Date: ${req.requestTime}`);
-    res.send(filteredUsersData);
-})
+    router.get('/', async (req, res) => {
+        const users = await User.find({});
+        const filteredUsersData = users.map(user => ({ username: user.username, weight: user.weight || null, height: user.height || null, email: user.email || '', id: user._id }));
+        console.log(`Date: ${req.requestTime}`);
+        res.send(filteredUsersData);
+    })
 
-router.post('/register', wrapAsync(async (req, res, next) => {
-    console.log(req.body);
-    const { username, password } = req.body;
-    const findUser = await User.findOne({ username });
-    if(!findUser) {
-        const hash = await hashPassword(password);
-        const user = {
-            username,
-            password: hash
-        }
-        if(req.body.email) user.email = req.body.email;
-        if(req.body.weight) user.weight = req.body.weight;
-        if(req.body.height) user.height = req.body.height;
-        const newUser = new User(user);
-        await newUser.save();
-        req.session.user_id = user._id;
-        res.send({result: 'Succesfully saved!'});
-        
-    }else {
-        res.status(401).send({message: 'Username already exists!' });
-    }
-}))
-
-router.post('/login', wrapAsync(async (req, res, next) => {
-    const { username, password } = req.body;
-    const user = await User.findOne({ username });
-    if(!user) {
-        res.status(401).send({message: 'Incorrect username or password!' });
-    }else {
-        const validPass = await bcrypt.compare(password, user.password);
-        if(validPass) {
-            const {id: sub, username} =  { id: user._id, username: user.username };
-            const currentDate = new Date();
-            const expirationDate = new Date(currentDate.getTime() + 60 * 60 * 1000); // 1 hora en milisegundos
-            const timestampInSeconds = expirationDate.getTime(); // Convertir a segundos
-            const accessToken = jwt.sign({
-                sub, 
+    router.post('/register', wrapAsync(async (req, res, next) => {
+        console.log(req.body);
+        const { username, password } = req.body;
+        const findUser = await User.findOne({ username });
+        if(!findUser) {
+            const hash = await hashPassword(password);
+            const user = {
                 username,
-                exp: timestampInSeconds
-            }, secret);
-            console.log('logged in!!');
-            //const token = twl.sign({id: sub, username: user.username, exp: Date.now() + 60 * 1000}, secret)
+                password: hash
+            }
+            if(req.body.email) user.email = req.body.email;
+            if(req.body.weight) user.weight = req.body.weight;
+            if(req.body.height) user.height = req.body.height;
+            const newUser = new User(user);
+            await newUser.save();
             req.session.user_id = user._id;
-            res.status(200).send({message: 'Succesfully logged in!', accessToken });
+            res.send({result: 'Succesfully saved!'});
+            
         }else {
-            res.status(401).send({message: 'Incorrect username or password!' });
+            res.status(401).send({message: 'Username already exists!' });
         }
-    }
-}))
+    }))
 
-router.post('/logout', wrapAsync(async (req, res, next) => {
-    req.session.user_id = null;
-}))
+    router.post('/login', wrapAsync(async (req, res, next) => {
+        const { username, password } = req.body;
+        const user = await User.findOne({ username });
+        if(!user) {
+            res.status(401).send({message: 'Incorrect username or password!' });
+        }else {
+            const validPass = await bcrypt.compare(password, user.password);
+            if(validPass) {
+                const {id: sub, username} =  { id: user._id, username: user.username };
+                const currentDate = new Date();
+                const expirationDate = new Date(currentDate.getTime() + 60 * 60 * 1000); // 1 hora en milisegundos
+                const timestampInSeconds = expirationDate.getTime(); // Convertir a segundos
+                const accessToken = jwt.sign({
+                    sub, 
+                    username,
+                    exp: timestampInSeconds
+                }, secret);
+                console.log('logged in!!');
+                //const token = twl.sign({id: sub, username: user.username, exp: Date.now() + 60 * 1000}, secret)
+                req.session.user_id = user._id;
+                res.status(200).send({message: 'Succesfully logged in!', accessToken });
+            }else {
+                res.status(401).send({message: 'Incorrect username or password!' });
+            }
+        }
+    }))
 
-router.get('/:id', wrapAsync(async (req, res, next) => {
-    const { id } = req.params;
-    const user = await User.findById(id);
-    if(!user) {
-        throw new AppError('User not found', 404);
-    }
-    res.send(user);
-}))
+    router.post('/logout', wrapAsync(async (req, res, next) => {
+        req.session.user_id = null;
+    }))
 
-// la diferencia entre put y patch es que put actualiza todo 
-router.put('/:id', wrapAsync(async (req, res) => {
-    const { id } = req.params;
-    const updatedUser = await User.findByIdAndUpdate(id, req.body, { runValidators: true, new: true });
-    res.send(updatedUser);
-}))
+    router.get('/:id', wrapAsync(async (req, res, next) => {
+        const { id } = req.params;
+        const user = await User.findById(id);
+        if(!user) {
+            throw new AppError('User not found', 404);
+        }
+        res.send(user);
+    }))
 
-router.delete('/:id', async (req, res) => {
-    const { id } = req.params;
-    const deletedUser = await User.findByIdAndDelete(id);
-    res.send(deletedUser);
-})
+    // la diferencia entre put y patch es que put actualiza todo 
+    router.put('/:id', wrapAsync(async (req, res) => {
+        const { id } = req.params;
+        const updatedUser = await User.findByIdAndUpdate(id, req.body, { runValidators: true, new: true });
+        res.send(updatedUser);
+    }))
 
-module.exports = router;
+    router.delete('/:id', async (req, res) => {
+        const { id } = req.params;
+        const deletedUser = await User.findByIdAndDelete(id);
+        res.send(deletedUser);
+    })
+
+    module.exports = router;
